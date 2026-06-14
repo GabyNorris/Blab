48e67eb7fb5b204362327c676c5f8d717ed5fccc
GabyNor05 <gabynorris616@gmail.com>
2026-06-14 23:02:26 +0200

    feat(messages): set up model + controller + route

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 // 2. IMPORT ROUTES AND DB AFTER LIBRARIES
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js"; 
 
 const app = express();
@@ -15,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
